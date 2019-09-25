6ba489bf389ce937f68e05cc8d2568ba35cb76bd
German <mpauluk@est.frba.utn.edu.ar>
2019-09-24 22:31:01 -0300

    adding showable commits for every repo detected

--- git.js
+++ git.js
@@ -2,13 +2,39 @@ var Octokit = require("@octokit/rest");
 
 var octokit = new Octokit();
 
+var author = 'matiasimartin'
+
 var repos = Promise.resolve(octokit.repos.listForUser({
-  username: 'germauricio'
+  username: author
 }));
 
+
+
 repos.then(function(listOfRepos) {
-	var names = listOfRepos.data
-	for(var i=0; i< names.length; i++){
-		console.log(names[i].name);
+	var names = listOfRepos.data;
+
+	console.log("Lista de repos de "+ author +":\n")
+	for(var i=0; i < names.length ; i++){
+		
+		console.log(names[i].name + "\n")
+	
+		var userCommits = Promise.resolve(octokit.repos.listCommits({
+			owner: author,
+			repo: names[i].name,
+		}));
+
+		userCommits.then(function(listOfCommits){
+			var commit = listOfCommits.data;
+
+			for(var j=0; j < commit.length; j++){
+				console.log("Commit n°" + j + " :")
+				console.log(commit[j].commit.message)
+		}
+		});
 	}
+	
 });
+
+
+
+
